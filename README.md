0bc828db099a93d5e8c4f2216858fb92807f6087
Jixing Fu <bxlxtl@gmail.com>
2020-12-03 11:46:49 +0800

    yac

--- README.md
+++ README.md
@@ -21,11 +21,16 @@ sudo bash init_nrf52tools.sh
 
 
 
+
+
 ### 在VSCODE中安装插件
 1. 跨系统编辑代码
-[Remote-WSL插件](https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-wsl)
+[Remote-WSL插件](https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-wsl)  
 
-2. 在线调试工具
-[CORTEX-DEBUG](https://marketplace.visualstudio.com/items?itemName=marus25.cortex-debug)
-
+2. 在线调试工具  
+[CORTEX-DEBUG](https://marketplace.visualstudio.com/items?itemName=marus25.cortex-debug)  
+配置gcc路径：    
+````
+"cortex-debug.armToolchainPath": "/opt/gcc-arm-none-eabi-9-2019-q4-major/bin/"
+````
 
--- init_nrf52tools.sh
+++ init_nrf52tools.sh
@@ -15,12 +15,27 @@ apt-get update && apt-get -y install \
 
 
 # download  gcc
+
 NRF_TOOLCHAIN=https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2
 wget -P /opt -q --show-progress $NRF_TOOLCHAIN
-tar -xvjf /opt/gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2 -o /opt
+tar -xvjf /opt/gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2 -C /opt
 
 # download SDKs
 NORDIC_SDK=https://www.nordicsemi.com/-/media/Software-and-other-downloads/SDKs/nRF5/Binaries/nRF5SDK1702d674dde.zip
 wget -P /opt -q --show-progress $NORDIC_SDK
 unzip /opt/nRF5SDK1702d674dde.zip -d /opt
 
+# install nrfutils
+sudo apt install python-pip
+pip install nrfutil
+
+
+# install jlink for debugger & download
+### 安装烧录工具 pynrfjprog
+#pip install pynrfjprog ## 太慢
+
+#改为：
+wget -P /opt https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-command-line-tools/sw/Versions-10-x-x/10-12-0/pynrfjprog10120.zip
+cd /opt
+unzip pynrfjprog10120.zip
+python /opt/pynrfjprog-10.12.0/setup.py install
